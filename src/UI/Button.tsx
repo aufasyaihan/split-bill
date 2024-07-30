0634aa80c5462257e4e261b9c13c9d12dafa1b67
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-30 22:33:40 +0700

    chore: Update Button component to include HTML button attributes

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,12 +1,13 @@
-import { FC, ReactNode } from "react";
+import { ButtonHTMLAttributes, FC, ReactNode } from "react";
 
-interface Props {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
 }
 
-const Button: FC<Props> = ({ children }) => {
+const Button: FC<Props> = ({ children, ...props }) => {
   return (
     <button
+      {...props}
       className="bg-zinc-300 px-5 py-2 rounded hover:bg-zinc-400"
     >
       {children}
