01ef0b8ef4f487d030816aecf716c03cdd4d229f
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-08-08 15:01:02 +0700

    refactor: Update FormSplitBill component to dynamically display the name of the current user

--- src/components/FormSplitBill.tsx
+++ src/components/FormSplitBill.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../UI/Button";
-import { Friends } from '../lib/friendlist'
+import { Friends } from "../lib/friendlist";
 
 interface Input {
   totalAmount: number;
   yourAmount: number;
-  user: string;
+  user: number;
 }
 
 interface Props {
@@ -15,10 +15,17 @@ interface Props {
 }
 
 const FormSplitBill: FC<Props> = ({ currUser, friends }) => {
-  const { register, handleSubmit } = useForm<Input>();
+  const { register, handleSubmit, watch } = useForm<Input>({
+    mode: "onChange",
+  });
   const onSubmitForm = (data: Input) => {
     console.log(data);
   };
+
+  const totalAmount = watch("totalAmount", 0);
+  const yourAmount = watch("yourAmount", 0);
+  const otherAmount = (totalAmount || 0) - (yourAmount || 0);
+
   const data = friends!.find((friend) => friend.id === currUser);
   return (
     <section className="flex gap-3 items-center justify-center h-72 my-56">
@@ -26,20 +33,22 @@ const FormSplitBill: FC<Props> = ({ currUser, friends }) => {
         onSubmit={handleSubmit(onSubmitForm)}
         className="flex flex-col bg-stone-200 w-full px-2 py-2 rounded"
       >
-        <h3 className="text-3xl font-semibold">Patungan bareng si {data?.name}</h3>
+        <h3 className="text-3xl font-semibold">
+          Patungan bareng si {data?.name}
+        </h3>
         <div className="flex flex-col group">
           <label htmlFor="totalAmount">Total Tagihan</label>
           <input
-            type="text"
-            {...register("totalAmount")}
+            type="number"
+            {...register("totalAmount", { valueAsNumber: true })}
             className="px-2 rounded-md bg-stone-100 border-stone-400 border-2 focus:outline-none focus:ring focus:ring-stone-300"
           />
         </div>
         <div className="flex flex-col group">
           <label htmlFor="yourAmount">Tagihan Anda</label>
           <input
-            type="text"
-            {...register("yourAmount")}
+            type="number"
+            {...register("yourAmount", { valueAsNumber: true })}
             className="px-2 rounded-md bg-stone-100 border-stone-400 border-2 focus:outline-none focus:ring focus:ring-stone-300"
           />
         </div>
@@ -47,6 +56,7 @@ const FormSplitBill: FC<Props> = ({ currUser, friends }) => {
           <label htmlFor="yourAmount">Tagihan {data?.name}</label>
           <input
             type="text"
+            value={otherAmount}
             className="px-2 rounded-md bg-stone-300 border-stone-400 border-2 focus:outline-none focus:ring focus:ring-stone-300"
             disabled
           />
@@ -57,7 +67,7 @@ const FormSplitBill: FC<Props> = ({ currUser, friends }) => {
             {...register("user")}
             className="px-2 rounded-md bg-stone-100 border-stone-400 border-2 focus:outline-none focus:ring focus:ring-stone-300"
           >
-            <option value="You">Kamu</option>
+            <option value={1}>Kamu</option>
             <option value={data?.id}>{data?.name}</option>
           </select>
         </div>
