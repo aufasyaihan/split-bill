c2a8028ae995e7d6cc5483f2e738e00f08013491
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-08-08 14:05:57 +0700

    refactor: Update FormSplitBill component to dynamically display the name of the current user

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import FriendList from "./components/FriendList";
 import initialFriends from "./lib/friendlist";
 
 function App() {
-  const [formVisible, setFormVisible] = useState<number | null>(null);
+  const [userId, setUserId] = useState<number | null>(null);
   const handleFormVisible = (id: number) => {
-    setFormVisible((prev) => (prev === id ? null : id));
+    setUserId((prev) => (prev === id ? null : id));
   };
+  console.log(userId);
+
   return (
     <>
       <section className="min-h-[66vh] grid grid-cols-2 gap-16 items-start">
@@ -15,7 +17,7 @@ function App() {
           friends={initialFriends}
           onFormVisibleToggle={handleFormVisible}
         />
-        {formVisible && <FormSplitBill />}
+        {userId && <FormSplitBill currUser={userId} />}
       </section>
     </>
   );
--- src/components/FormSplitBill.tsx
+++ src/components/FormSplitBill.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../UI/Button";
+import initialFriends from '../lib/friendlist'
 
 interface Input {
   totalAmount: number;
@@ -8,18 +9,23 @@ interface Input {
   user: string;
 }
 
-const FormSplitBill: FC = () => {
+interface Props {
+  currUser: number;
+}
+
+const FormSplitBill: FC<Props> = ({ currUser }) => {
   const { register, handleSubmit } = useForm<Input>();
   const onSubmitForm = (data: Input) => {
     console.log(data);
   };
+  const data = initialFriends.find((friend) => friend.id === currUser);
   return (
     <section className="flex gap-3 items-center justify-center h-72 my-56">
       <form
         onSubmit={handleSubmit(onSubmitForm)}
         className="flex flex-col bg-stone-200 w-full px-2 py-2 rounded"
       >
-        <h3 className="text-3xl font-semibold">Patungan bareng si X</h3>
+        <h3 className="text-3xl font-semibold">Patungan bareng si {data?.name}</h3>
         <div className="flex flex-col group">
           <label htmlFor="totalAmount">Total Tagihan</label>
           <input
@@ -37,7 +43,7 @@ const FormSplitBill: FC = () => {
           />
         </div>
         <div className="flex flex-col group">
-          <label htmlFor="yourAmount">Tagihan X</label>
+          <label htmlFor="yourAmount">Tagihan {data?.name}</label>
           <input
             type="text"
             className="px-2 rounded-md bg-stone-300 border-stone-400 border-2 focus:outline-none focus:ring focus:ring-stone-300"
